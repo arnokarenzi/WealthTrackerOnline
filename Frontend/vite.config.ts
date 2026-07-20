4aeaa23a39c07b3d85018eee361e210ea12b3446
Karenzi Arnold <arnokarenzi@gmail.com>
2026-07-20 09:19:50 +0200

    Fix Vite base path for Vercel root deployment

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "fintrack",
+  base: "/",
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['ts-big-decimal']
+    exclude: ["ts-big-decimal"],
   },
-})
+});
